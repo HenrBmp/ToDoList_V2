64f270e373cfa788fb573dbf7656ee2e01708796
HenrBmp <henriquerodr20@gmail.com>
2026-02-22 17:20:45 -0300

    corrigi o pop up sendo gerado incorrretamente e erros menores

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import { PopUp } from "./scripts/pop-up.js";
-if (!localStorage.getItem(username)) {
+if (!localStorage.getItem("username")) {
     PopUp(
         "Insira um nome.",
         "Pronto",
-        () => {
+        async () => {
             // gerar input de texto
+            return [ /* input aqui */];
         },
         () => {
             // capturar valor do input e mandar para o span
         }
     );
+}
+if (!localStorage.getItem("tasklist")) {
+    localStorage.setItem("tasklist", "[]");
 }
--- scripts/pop-up.js
+++ scripts/pop-up.js
@@ -1,15 +1,19 @@
 async function PopUp(msg, sendLabel, inputCallback, sendActionCallback) {
     const BLUR_DIV = document.createElement("div");
     BLUR_DIV.id = "blur-box";
+    document.getElementById("root").appendChild(BLUR_DIV);
+
     const POP_UP = document.createElement("div");
-    POP_UP.setAttribute("id", "pop-up");
+    POP_UP.id = "pop-up";
     BLUR_DIV.appendChild(POP_UP);
 
     const HEAD_MSG = document.createElement("h2");
     HEAD_MSG.id = "pop-up-msg";
     HEAD_MSG.innerHTML = msg;
+    POP_UP.appendChild(HEAD_MSG);
 
-    inputCallback();
+    const inputs = await inputCallback();
+    inputs.forEach(input => POP_UP.appendChild(input));
 
     const ACTION_BUTTON = document.createElement("button");
     ACTION_BUTTON.id = "sendAction-btn";
@@ -21,14 +25,16 @@ async function PopUp(msg, sendLabel, inputCallback, sendActionCallback) {
             document.getElementById("blur-box").remove();
         }
     );
+    POP_UP.appendChild(ACTION_BUTTON);
 
     const CANCEL_BUTTON = document.createElement("button");
-    CANCEL_BUTTON.id = "calcel-btn";
+    CANCEL_BUTTON.id = "cancel-btn";
     CANCEL_BUTTON.innerHTML = "Cancelar";
     CANCEL_BUTTON.addEventListener(
         "click",
         () => document.getElementById("blur-box").remove()
     );
+    POP_UP.appendChild(CANCEL_BUTTON);
 };
 
 export { PopUp };
