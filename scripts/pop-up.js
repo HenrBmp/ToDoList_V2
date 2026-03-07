b2af31721aa97af001e6d724681fa93b76bc09a0
HenrBmp <henriquerodr20@gmail.com>
2026-03-07 19:55:32 -0300

    os pop ups agora podem criar apenas um botão

--- scripts/pop-up.js
+++ scripts/pop-up.js
@@ -15,26 +15,38 @@ async function PopUp(msg, sendLabel, inputCallback, sendActionCallback) {
     const inputs = await inputCallback();
     inputs.forEach(input => POP_UP.appendChild(input));
 
-    const ACTION_BUTTON = document.createElement("button");
-    ACTION_BUTTON.id = "sendAction-btn";
-    ACTION_BUTTON.innerHTML = sendLabel;
-    ACTION_BUTTON.addEventListener(
-        "click",
-        () => {
-            sendActionCallback();
-            document.getElementById("blur-box").remove();
-        }
-    );
-    POP_UP.appendChild(ACTION_BUTTON);
+    if (sendLabel) {
+        const ACTION_BUTTON = document.createElement("button");
+        ACTION_BUTTON.id = "sendAction-btn";
+        ACTION_BUTTON.innerHTML = sendLabel;
+        ACTION_BUTTON.addEventListener(
+            "click",
+            () => {
+                sendActionCallback();
+                document.getElementById("blur-box").remove();
+            }
+        );
+        POP_UP.appendChild(ACTION_BUTTON);
 
-    const CANCEL_BUTTON = document.createElement("button");
-    CANCEL_BUTTON.id = "cancel-btn";
-    CANCEL_BUTTON.innerHTML = "Cancelar";
-    CANCEL_BUTTON.addEventListener(
-        "click",
-        () => document.getElementById("blur-box").remove()
-    );
-    POP_UP.appendChild(CANCEL_BUTTON);
+        const CANCEL_BUTTON = document.createElement("button");
+        CANCEL_BUTTON.id = "cancel-btn";
+        CANCEL_BUTTON.innerHTML = "Cancelar";
+        CANCEL_BUTTON.addEventListener(
+            "click",
+            () => document.getElementById("blur-box").remove()
+        );
+        POP_UP.appendChild(CANCEL_BUTTON);
+    } else {
+        const CONFIRM_BUTTON = document.createElement("button");
+        CONFIRM_BUTTON.id = "confirm-btn";
+        CONFIRM_BUTTON.innerHTML = "Ok";
+        CONFIRM_BUTTON.addEventListener(
+            "click",
+            () => document.getElementById("blur-box").remove()
+        );
+        POP_UP.appendChild(CONFIRM_BUTTON);
+    }
+
 };
 
 export { PopUp };
