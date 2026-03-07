e73eb3dac48bb4c24309f6d019075b009b08ce19
HenrBmp <henriquerodr20@gmail.com>
2026-03-07 20:21:59 -0300

    adicionei uma verificação contra tarefas repetidas

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { PopUp } from "./scripts/pop-up.js";
-import { addTask } from "./scripts/add-task.js";
 if (!localStorage.getItem("username")) {
     PopUp(
         "Insira um nome.",
@@ -45,7 +44,7 @@ RESET_TASKS_BTN.addEventListener(
 const CREATE_TASK_BTN = document.getElementById("create-btn");
 CREATE_TASK_BTN.addEventListener(
     "click",
-    function ExtractTaksInfo(){
+    function addTask(){
         event.preventDefault();
         const DESC_INPUT = document.getElementById("description-entry");
         const DATE_INPUT = document.getElementById("validity-entry");
@@ -61,12 +60,20 @@ CREATE_TASK_BTN.addEventListener(
         }
         const DATE_OBJECT = DATE_INPUT.valueAsDate;
         const NOW = new Date;
-        const TASK_OBJECT = {
+        const THIS_TASK_OBJECT = {
             "description": DESC_INPUT.value,
             "epoch": DATE_OBJECT.getTime(),
+            "subOrder": 0,
             "isDone": false,
             "isOverdue": DATE_OBJECT.getTime() < NOW.getTime() ? false : true
         };
-        addTask(TASK_OBJECT);
+        const tasklist = JSON.parse(localStorage.getItem("tasklist"));
+        tasklist.forEach(target_task => {
+            if (
+                THIS_TASK_OBJECT.description === target_task.description &&
+                THIS_TASK_OBJECT.epoch === target_task.epoch &&
+                THIS_TASK_OBJECT.subOrder === target_task.subOrder
+            ) PopUp("Esta tarefa já existe.");
+        });
     }
 )
