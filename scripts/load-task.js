07e55940d735ae75d3ff4aad75bcfc634670f2b3
HenrBmp <henriquerodr20@gmail.com>
2026-03-31 20:00:17 -0300

    fix/isOverdue e agora e atualizado em load-tasks

--- scripts/load-task.js
+++ scripts/load-task.js
@@ -6,6 +6,8 @@ export async function loadTasks(tasksJSON) {
         task_div.id = `${task.epoch}-${task.subOrder}`;
         task_div.classList.add("task-item");
         if (task.isDone) task_div.classList.add("done");
+        const now = new Date;
+        if (!task.isOverdue && task.epoch < now.getTime()) task.isOverdue = true;
         if (task.isOverdue) task_div.classList.add("overdue");
         const done_buttton = await createSVG("M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z");
         done_buttton.classList.add("done-check");
